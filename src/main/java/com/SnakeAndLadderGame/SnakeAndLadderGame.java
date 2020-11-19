9c46de76e000ca758060a61c5c107afea920e4cf
aakash2809 <aakashrajak289@gmail.com>
2020-11-19 05:32:11 +0530

    Game initiation

---
+++ src/main/java/com/SnakeAndLadderGame/SnakeAndLadderGame.java
@@ -0,0 +1,14 @@
+package com.SnakeAndLadderGame;
+
+/*HERE GAME IS INITIATED FOR SINGLE PLAYER */
+
+public class SnakeAndLadderGame {
+
+	public static void main(String[] args) {
+
+		System.out.println("        welcome to Snake and Ladder Game     ");
+		System.out.println("---------------------------------------------");
+
+	}
+
+}
